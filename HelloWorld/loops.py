7ff4a700e973be03fe2a667069c60bdc47403545
hamdysj <hamdysj@gmail.com>
2022-10-04 17:02:53 +0100

    Update0410

---
+++ HelloWorld/loops.py
@@ -0,0 +1,34 @@
+#While Loop
+
+#val = int(input("Enter an Even Number"))
+#while val%2 != 0 :
+#    val = int(input("Enter an Even Number"))
+#else :
+ #   print("%d is an even number" % val)
+
+
+#For Loop
+#x = (2, 5,"Hello", "World") #Tuple
+#for j in x:
+#    print(f"{j} ", end="")
+
+#Nested Loop
+#i = [[1,2,3],["a","b","c"]]
+#for x in i:
+#    for j in x:
+#        print(f"{j} ", end="")
+#    print()
+
+#break
+for i in [1,2,56,48,25] :
+    if i>50 :
+        break
+    print(i)
+
+#continue
+for i in [1,2,56,48,25] :
+    if i>50 :
+        continue
+    print(i)
+
+
