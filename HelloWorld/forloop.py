de97c12709d2cd135832f0ab8a3e5bbc6e6831f3
hamdysj <hamdysj@gmail.com>
2022-10-06 11:18:58 +0100

    Update0610

--- HelloWorld/forloop.py
+++ HelloWorld/forloop.py
@@ -3,8 +3,38 @@
 #    print(i)
 
 #pattern
-n = int(input('Enter a Number: '))
-for i in range(1,n+1):
-    for j in range(1,i+1):
-        print(j,end='')
-    print()
+#n = int(input('Enter a Number: '))
+#for i in range(1,n+1):
+#    for j in range(1,i+1):
+ #       print(j,end='')
+ #   print()
+
+#Matrices
+row = 2
+col = 2
+val = []
+x=[]
+y = []
+
+for i in range(0, row):
+    for j in range(0, col):
+        val.insert(j, int(input('Enter %d x %d number: ' %(i,j))))
+    x.insert(i,val)
+    val=[]
+for i in range(0, row):
+    for j in range(0, col):
+        val.insert(j, int(input('Enter %d x %d number: ' %(i,j))))
+    y.insert(i,val)
+    val=[]
+
+sum=[]
+for i in range(0, row):
+    for j in range(0, col):
+        val.insert(j, x[i][j] + y[i][j])
+    sum.insert(i,val)
+    val=[]
+
+print(x)
+print(y)
+print(sum)
+
--- HelloWorld/list_tuples.py
+++ HelloWorld/list_tuples.py
@@ -12,8 +12,25 @@ x3 = x+x2
 #print(x[::-1])#returns the values in reverse order
 #print(x3[1:7:2])
 
-x4=['a']*4
-print(x4)
+#var = list("Furious")
+#print(var)
+
+#first, *second = x
+#print(first)
+#print(second)
+
+#x4=['a']*4
+#print(x4)
+
+#x.append('NewValue') #add a single element
+#print(x)
+
+#x.extend(x3) #add an entire list
+#print(x)
+
+x.insert(2, 3) #to add value to a list
+x.remove('get') #to remove a value form the list
+print(x)
 
 
 
